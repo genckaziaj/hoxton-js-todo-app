a7ce898bd1ee4e76a7985a82f11dce75c444047f
Genci23 <genckaziaj@yahoo.com>
2022-06-27 02:16:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+Description
+In this exercise we explore how to build a simple todo app, one of the most common starter projects whenever you are learning a new technology. The great thing about todo apps is that you already know how they work, so you can focus on how to build it rather than what to build.
+
+Deliverables
+A user can:
+- View incomplete todos
+- When the "Show completed todos" box is checked, view completed todos as well
+- Enter a new todo, which will initially show as incomplete
+- Click a todo to toggle its completion
+
+Instructions
+- Start this project from scratch, write whatever HTML, CSS & JS you need
+- Create a state object
+- Create action functions that update state
+- Create render functions that read from state
+
+If you're not feeling it, feel free to use this template: https://codesandbox.io/s/todo-state-template-ejekb?from-embed=&file=/src/index.js
+
+Tips
+- Try starting with state. Create the state for the todos, then a function to toggle a todo's completed state, a function to add a new todo, etc.
+- You can test these functions before you even render anything on the page, just by calling the functions in your js file.
+*/
